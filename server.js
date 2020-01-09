f688a5aa650118dc6585a28597c5d473818d0a37
Rakesh Keerthi <rkeerthi@salesforce.com>
2020-01-09 14:41:46 +0530

    Added CORS

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var fs = require('fs');
 var processing = require('./app/routes/process');
 
 //git diff HEAD FETCH_HEAD
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.post('/notes', bodyParser, (req, res) => {
     res.send({
         'new': processing.inputFromFrontEnd(req.body)
